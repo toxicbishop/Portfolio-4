cef485f50dcba97bf777a35740b57abbd50cf25c
toxicbishop <pranavarun19@gmail.com>
2026-07-24 23:49:35 +0530

    fix: resolve all remaining security advisories (postcss, sharp, @hono/node-server)

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,10 +2,10 @@ module.exports = {
   hooks: {
     readPackage(pkg) {
       if (pkg.dependencies && pkg.dependencies.postcss) {
-        pkg.dependencies.postcss = '8.5.10';
+        pkg.dependencies.postcss = '8.5.23';
       }
       if (pkg.devDependencies && pkg.devDependencies.postcss) {
-        pkg.devDependencies.postcss = '8.5.10';
+        pkg.devDependencies.postcss = '8.5.23';
       }
       return pkg;
     }
